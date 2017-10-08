0f8204269cbba86be441b577e48a7ddb2e292805
Alex Ray <1alexray@gmail.com>
2017-10-08 10:51:48 -0600

    Removed unnecessary function

--- StumpSpeechScraper.py
+++ StumpSpeechScraper.py
@@ -3,11 +3,14 @@ import urllib.request
 import urllib.parse
 from bs4 import BeautifulSoup
 
+# Get speech as list given a url
 def get_speech(url):
     try:
+        # Getting text of page
         content = urllib.request.urlopen(url).read()
         soup = BeautifulSoup(content, 'html.parser')
         txt = str(soup)
+
         # speech lies between up_partition and down_partition
         up_partition = '<span class="displaytext">'
         down_partition = '<span class="displaynotes">'
@@ -20,6 +23,7 @@ def get_speech(url):
         txt = re.sub('[^a-z\ \']+', " ", txt)
         words = list(txt.split())
         return words
+
     except Exception as e:
         return "Exception occurred \n" +str(e)
 
@@ -39,14 +43,12 @@ def get_relevant_urls(base_url):
     except Exception as e:
         return "Exception occurred \n" +str(e)
 
-def is_relative(url):
-    return not bool(urlparse.urlparse(url).netloc)
-
 def main():
     clinton_remark_urls = get_relevant_urls('http://www.presidency.ucsb.edu/2008_election_speeches.php?candidate=70&campaign=2008CLINTON&doctype=5000')
     clinton_remarks = []
     for e in clinton_remark_urls:
         clinton_remarks.append(get_speech(e))
     print(len(clinton_remarks))
+    
 if __name__ == '__main__':
     main()
