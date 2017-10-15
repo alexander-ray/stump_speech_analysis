729c5ad03f25884ef7576e357f83c83596f07815
Alex Ray <1alexray@gmail.com>
2017-10-14 19:21:39 -0600

    Started with SGD classifier

--- driver.py
+++ driver.py
@@ -1,5 +1,14 @@
 import json
 import os
+
+import pickle
+import numpy as np
+from urllib.request import urlopen
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import accuracy_score, precision_score
+import matplotlib.pyplot as plt
+
 from speech_scraper import remarks_driver
 
 def main():
@@ -20,18 +29,32 @@ def main():
     if (not exists):
         remarks_driver()
 
-    for key, value in filenames.items():
+    for key, _ in filenames.items():
         with open(key) as json_file:
-            value = json.load(json_file)
+            filenames[key] = json.load(json_file)
 
-    with open('clinton_remarks_train') as json_file:
-        clinton_remarks_train = json.load(json_file)
-    with open('obama_remarks_train') as json_file:
-        obama_remarks_train = json.load(json_file)
+    train = filenames['clinton_remarks_train'] + filenames['obama_remarks_train']
+    D = np.array(train)
+    Y = D[:, 0]
+    v = DictVectorizer(sparse=True)
+    X = v.fit_transform(D[:,1])
+    feature_names = np.asarray(v.get_feature_names())
 
-    print(len(clinton_remarks_train))
-    print(len(obama_remarks_train))
-    print(obama_remarks_train[0])
+    test = filenames['clinton_remarks_test'] + filenames['obama_remarks_test']
+    D_test = np.array(test)
+    Y_test = D_test[:,0]
+    X_test = v.transform(D_test[:,1])
 
+    print("Number of training instances: %d" % (D.shape[0]))
+    print("Number of test instances: %d" % (D_test.shape[0]))
+
+    alpha = 0.001
+    classifier = SGDClassifier(loss='log', max_iter=1000, tol=1.0e-12, random_state=123, alpha=alpha)
+    classifier.fit(X, Y)
+
+    print("\nFeature weights:")
+    args = np.argsort(classifier.coef_[0])
+    for a in args:
+        print(" %s: %0.4f" % (feature_names[a], classifier.coef_[0][a]))
 if __name__ == '__main__':
     main()
