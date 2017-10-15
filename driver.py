253969e6c125b1e054693037374e0cdf5b03ea3d
Alex Ray <1alexray@gmail.com>
2017-10-14 18:42:55 -0600

    Added filename checking, refactoring, segregated training and testing data

--- driver.py
+++ driver.py
@@ -1,14 +1,37 @@
 import json
+import os
+from speech_scraper import save_remarks
 
 def main():
-    with open('clinton_remarks') as json_file:
-        clinton_remarks = json.load(json_file)
-    with open('obama_remarks') as json_file:
-        obama_remarks = json.load(json_file)
+    clinton_remarks_test = []
+    clinton_remarks_train = []
+    obama_remarks_test = []
+    obama_remarks_train = []
+    filenames = {'clinton_remarks_test': clinton_remarks_test,
+                'clinton_remarks_train': clinton_remarks_train,
+                'obama_remarks_test': obama_remarks_test,
+                'obama_remarks_train': obama_remarks_train}
 
-    print(len(clinton_remarks))
-    print(len(obama_remarks))
-    print(obama_remarks[0])
+    exists = True
+    for key, _ in filenames.items():
+        if (not os.path.isfile('./' + key)):
+            exists = False
+
+    if (not exists):
+        save_remarks()
+
+    for key, value in filenames.items():
+        with open(key) as json_file:
+            value = json.load(json_file)
+
+    with open('clinton_remarks_train') as json_file:
+        clinton_remarks_train = json.load(json_file)
+    with open('obama_remarks_train') as json_file:
+        obama_remarks_train = json.load(json_file)
+
+    print(len(clinton_remarks_train))
+    print(len(obama_remarks_train))
+    print(obama_remarks_train[0])
 
 if __name__ == '__main__':
     main()
